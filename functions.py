e971891a910958e0e79acd209916ce3a6a6cdafa
Nabanita Saha <nabanitasaha131094@gmail.com>
2023-10-07 22:52:15 +0530

    whole project done

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = "C:\\Users\\naban\\PycharmProjects\\pythonProject_todolist\\todo.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    '''
+    reads the todo text file in read mode and contains
+    all the values in todos_local list
+    '''
+    with open(filepath, "r") as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todo_arg, filepath=FILEPATH):
+    with open(filepath, "w") as file:
+        file.writelines(todo_arg)
